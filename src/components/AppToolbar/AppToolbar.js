7bef7872b411eb660f85815bc2995858540693d4
maksimus <gorlovmaks23@gmail.com>
2020-11-12 18:16:27 +0600

    Добавлена шапка для приложения

---
+++ src/components/AppToolbar/AppToolbar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import {Link} from "react-router-dom";
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles(theme => ({
+  mainLink: {
+    color: "inherit",
+    textDecoration: "none",
+    "&:hover": {
+      color: "inherit"
+    },
+    fontFamily: "Montserrat",
+    textTransform: "uppercase"
+  },
+  staticToolbar: {
+    marginBottom: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  },
+  container: {
+    display: "flex"
+  }
+}));
+
+const AppToolbar = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar>
+        <Toolbar>
+          <Container className={classes.container}>
+            <Typography variant="h6" className={classes.title}>
+              <Link to="/" className={classes.mainLink}>Music</Link>
+            </Typography>
+          </Container>
+        </Toolbar>
+      </AppBar>
+      <Toolbar className={classes.staticToolbar}/>
+    </>
+  );
+};
+
+export default AppToolbar;
