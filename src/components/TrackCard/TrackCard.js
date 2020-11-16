30bc2ad17cf3ccfb7c34ae92838e4b8c4d672028
maksimus <gorlovmaks23@gmail.com>
2020-11-16 17:42:21 +0600

    HW#86 Создала кнопку для добавления треков в историю (только для залогиненных пользователей)

--- src/components/TrackCard/TrackCard.js
+++ src/components/TrackCard/TrackCard.js
@@ -3,17 +3,18 @@ import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
 import MusicNoteIcon from '@material-ui/icons/MusicNote';
 import PropTypes from "prop-types";
+import {useDispatch, useSelector} from "react-redux";
+import {addTrackToHistory} from "../../store/actions/trackHistoryActions";
 
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
-        minWidth: 1000
+        minWidth: 1200
     },
     demo: {
         backgroundColor: theme.palette.background.paper,
@@ -22,15 +23,33 @@ const useStyles = makeStyles((theme) => ({
         margin: theme.spacing(4, 0, 2),
     },
     item: {
-        width: 1000,
+        width: 1200,
         justifyContent: "space-between",
-        borderBottom: "1px solid #ccc",
+        border: "1px solid #ccc",
         margin: "0px auto"
+    },
+    text: {
+        width: "700px",
+        fontSize: 20
+    },
+    number: {
+        width: "50px"
+    },
+    duration: {
+        width: "40px",
+        marginRight: "10px"
+    },
+    avatar: {
+        backgroundColor: "#3f51b5"
     }
 }));
 
+
+
 const TrackCard = ({id,title,duration,number}) => {
     const classes = useStyles();
+    let userName = useSelector(state => state.users.user);
+    const dispatch = useDispatch();
 
     return (
         <div className={classes.root}>
@@ -40,19 +59,17 @@ const TrackCard = ({id,title,duration,number}) => {
                         <List>
                                 <ListItem id={id} className={classes.item}>
                                     <ListItemAvatar>
-                                        <Avatar>
+                                        <Avatar className={classes.avatar}>
                                             <MusicNoteIcon/>
                                         </Avatar>
                                     </ListItemAvatar>
-                                    <ListItemText
-                                        primary={number}
-                                    />
-                                    <ListItemText
-                                        primary={title}
-                                    />
-                                    <ListItemText
-                                        primary={duration}
-                                    />
+                                    <p className={classes.number}>{number}</p>
+                                    <p className={classes.text}>{title}</p>
+                                    <p className={classes.duration}>{duration}</p>
+                                    {userName ?
+                                        <button className="button-4" onClick={() => dispatch(addTrackToHistory(id))}><span>Add Track To History</span></button>
+                                        : null
+                                    }
                                 </ListItem>
                         </List>
                     </div>
