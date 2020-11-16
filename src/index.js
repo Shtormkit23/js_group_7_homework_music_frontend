f77dff23482d5c72123ae9d38ee36f5b6ad9173f
maksimus <gorlovmaks23@gmail.com>
2020-11-16 17:52:26 +0600

    HW#86 Обновлена главная структура приложения

--- src/index.js
+++ src/index.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from "react-router-dom";
-import {createStore, applyMiddleware, compose, combineReducers} from "redux";
-import {Provider} from "react-redux";
-import thunkMiddleware from 'redux-thunk'
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import {createBrowserHistory} from "history";
+import {connectRouter, routerMiddleware, ConnectedRouter} from "connected-react-router";
 import App from './App';
+import usersReducer from "./store/reducers/usersReducer";
 import musicReducer from "./store/reducers/musicReducer";
+import trackHistoryReducer from "./store/reducers/trackHistoryReducer";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const history = createBrowserHistory();
+
 const rootReducer = combineReducers({
     music: musicReducer,
+    users: usersReducer,
+    history: trackHistoryReducer,
+    router: connectRouter(history)
 });
 
-const store = createStore(
-    rootReducer,
-    composeEnhancers(applyMiddleware(thunkMiddleware))
-);
+const middleware = [
+    thunkMiddleware,
+    routerMiddleware(history)
+];
+
+const enhancers = composeEnhancers(applyMiddleware(...middleware));
+
+const store = createStore(rootReducer, enhancers);
 
 const app = (
     <Provider store={store}>
-        <BrowserRouter>
+        <ConnectedRouter history={history}>
             <App/>
-        </BrowserRouter>
+        </ConnectedRouter>
     </Provider>
 );
 
