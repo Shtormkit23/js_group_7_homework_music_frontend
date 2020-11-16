3c3fbd18ef2f2a7f0ae2105b21b976b2accd66e3
maksimus <gorlovmaks23@gmail.com>
2020-11-16 17:52:11 +0600

    HW#86 Добавлены новые роуты

--- src/App.js
+++ src/App.js
@@ -6,23 +6,32 @@ import AppToolbar from "./components/AppToolbar/AppToolbar";
 import Artists from "./container/Artists/Artists";
 import Albums from "./container/Albums/Albums";
 import Tracks from "./container/Tracks/Tracks";
+import Register from "./container/Register/Register";
+import Login from "./container/Login/Login";
+import {useSelector} from "react-redux";
+import TracksHistory from "./container/TrackHistory/TrackHistory";
 
-const App = () => (
+const App = () => {
+const user = useSelector(state => state.users.user);
+return(
     <>
         <CssBaseline />
-        <AppToolbar/>
+        <AppToolbar user={user}/>
         <main>
             <Container>
                 <Switch>
                     <Route path="/" exact component={Artists} />
                     <Route path="/albums" exact component={Albums}/>
                     <Route path="/tracks" exact component={Tracks}/>
+                    <Route path="/register" exact component={Register} />
+                    <Route path="/login" exact component={Login} />
+                    <Route path="/track_history" exact component={TracksHistory} />
                     <Route render={() => <h1>404 Not Found</h1>}/>
                 </Switch>
             </Container>
         </main>
     </>
-);
+)};
 
 export default App;
 
