c08787be5c1814ec04e92d485a03c0f31ff11a69
maksimus <gorlovmaks23@gmail.com>
2020-12-03 16:55:17 +0600

    Добавила функции для публикации и удаления альбомов, артистов, треков

--- src/store/actions/adminActions.js
+++ src/store/actions/adminActions.js
@@ -1,5 +1,6 @@
 import axios from "../../axiosApi";
 import {FETCH_ALBUMS_SUCCESS, FETCH_ARTISTS_SUCCESS, FETCH_FAILURE, FETCH_TRACKS_SUCCESS} from "../actionTypes";
+import {push} from "connected-react-router";
 
 const fetchAllArtistsSuccess = artists => {
     return {type: FETCH_ARTISTS_SUCCESS, artists};
@@ -20,7 +21,7 @@ const fetchFailure = error => {
 export const fetchAllArtists = () => {
     return async dispatch => {
         try {
-            await axios.get("/artists").then(response => {
+            await axios.get("/publish/artists").then(response => {
                 dispatch(fetchAllArtistsSuccess(response.data));
             })}catch(e) {
             if (e.response && e.response.data) {
@@ -61,6 +62,40 @@ export const fetchAllTracks = () => {
                 }
             });
         }catch(e) {
+            if (e.response && e.response.data) {
+                dispatch(fetchFailure(e.response.data));
+            } else {
+                dispatch(fetchFailure({global: "No internet"}));
+            }
+        }
+    };
+};
+
+export const publish = (item, id) => {
+    return async (dispatch, getState) => {
+        try {
+            const token = getState().users.user.token;
+            const headers = {'Authorization': token};
+            await axios.put(`/publish/${item}/${id}`, {}, {headers});
+            dispatch(push(`/${item}`));
+        } catch(e) {
+            if (e.response && e.response.data) {
+                dispatch(fetchFailure(e.response.data));
+            } else {
+                dispatch(fetchFailure({global: "No internet"}));
+            }
+        }
+    };
+};
+
+export const deleteItem = (item, id) => {
+    return async (dispatch, getState) => {
+        try {
+            const token = getState().users.user.token;
+            const headers = {'Authorization': token};
+            await axios.delete(`/deletion/${item}/${id}`, {headers});
+            dispatch(push(`/${item}`));
+        } catch(e) {
             if (e.response && e.response.data) {
                 dispatch(fetchFailure(e.response.data));
             } else {
