f61814610d52ab19779410294db9be28205af031
maksimus <gorlovmaks23@gmail.com>
2020-12-14 17:09:12 +0600

    HW#94 Добавила новый порт в случае тестового запуска

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,7 @@
-export const apiURL = "http://localhost:8001";
+let port = 8000;
+if(process.env.REACT_APP_NODE_ENV === "test") {
+    port = 8010;
+}
+
+export const apiURL = "http://localhost:" + port;
 export const fbAppId = "2717404738523499";
